d86feea386c1d0a18653e63e68c982ec194781a4
admin ekselen <admin@ekselen.id>
2021-04-01 20:05:49 +0700

    new golang fundamental

---
+++ hello/helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// development bisa menggunakan go run helloworld.go mempercepat proses
+// jika sudah production harus menggunakan go build helloworld.go
---
+++ tipedata/integer.go
@@ -0,0 +1,9 @@
+/**
+Tipe data integer:
+Tipe Data				Nilai Minimum			Nilai Maksimum
+int8					-128					127
+int16					-32768					32767
+int32					-2147483648				2147483647
+int64					-9223372036854772808	9223372036854772807
+
+**/
